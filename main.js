9fa70accb222f2ac4fee897fe2f41ccd9b51d0f9
Puong Lam Coder <147136667+puonghoccode@users.noreply.github.com>
2024-10-29 02:06:05 +0700

    add next button

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+// Wait for the document to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Set a timeout for 7 seconds (7000 milliseconds)
+    setTimeout(function() {
+        // Show the "Next" button after 7 seconds
+        document.getElementById('nextButton').style.display = 'block';
+    }, 7000);
+});
